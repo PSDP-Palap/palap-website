14c7b0de92b85f28dcffef9068c22cb3b761dbbe
Kuyjeng <peerapat0912470677@gmail.com>
2026-02-19 17:13:23 +0700

    Homepage
    Style with some animation but still not finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// 1. Import your Home component
+import Home from './Homepage.tsx' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* 2. Put it on the screen */}
+    <Home />
   </StrictMode>,
 )
